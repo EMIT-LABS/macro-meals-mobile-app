ae9fbf4b16ade3e7263530d62c55557025893afb
eli-codess <elikplim@emit-labs.com>
2026-02-13 10:40:48 +0000

    Updated MealIcons condition to switch case

--- src/utils/mealIcons.ts
+++ src/utils/mealIcons.ts
@@ -2,22 +2,29 @@ import React from 'react'
 import { IMAGE_CONSTANTS } from 'src/constants/imageConstants'
 
 function getMealIcons(meal: any) {
-  return (  meal === 'manual'
-   ? IMAGE_CONSTANTS.fireIcon
-   : meal ===
-       'barcode'
-      ? IMAGE_CONSTANTS.scanBarcodeIcon
-    : meal ===
-      'scanned'
-    ? IMAGE_CONSTANTS.scanMealIcon
-   : meal === 'ai_recipe'
-   ? IMAGE_CONSTANTS.aiRecipeIcon
-    :meal === 'search'
-    ? IMAGE_CONSTANTS.loggingSearchIcon
-    :meal === 'meal_finder'
-    ? IMAGE_CONSTANTS.mealFinderIcon
-      : IMAGE_CONSTANTS.fireIcon 
-  )
+    switch (meal) {
+    case 'manual':
+      return IMAGE_CONSTANTS.fireIcon;
+
+    case 'barcode':
+      return IMAGE_CONSTANTS.scanBarcodeIcon;
+
+    case 'scanned':
+      return IMAGE_CONSTANTS.scanMealIcon;
+
+    case 'ai_recipe':
+      return IMAGE_CONSTANTS.aiRecipeIcon;
+
+    case 'search':
+      return IMAGE_CONSTANTS.loggingSearchIcon;
+
+    case 'meal_finder':
+      return IMAGE_CONSTANTS.mealFinderIcon;
+
+    default:
+      return IMAGE_CONSTANTS.fireIcon;
+    }
+  
 }
 
 export default getMealIcons
